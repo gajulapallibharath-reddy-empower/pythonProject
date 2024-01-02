0212f245af674e93cc95babbaa3b0497dc1da382
Bharath <bharath588@gmail.com>
2024-01-02 15:11:12 +0530

    implicitwait added

--- day2/launchBrowser.py
+++ day2/launchBrowser.py
@@ -1,10 +1,23 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Chrome()
 driver.get("https://www.google.com")
 driver.maximize_window()
-time.sleep(5)
+driver.implicitly_wait(10)
+time.sleep(3)
+googleSearchBox = driver.find_element(By.NAME, "q")
+googleSearchBox.send_keys("bharath",Keys.ENTER)
+
+wait = WebDriverWait(driver, timeout=2)
+wait.until(expected_conditions.presence_of_element_located())
+# time.sleep(1)
+# googleSearchBox.send_keys(Keys.ENTER)
+time.sleep(1)
 
 driver.quit()
